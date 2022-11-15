b1b60345957a57bee27339d7455d4bee492b3f60
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2022-11-16 03:38:42 +0900

    共通の設定ファイル参照できるようにする #9

---
+++ src/SnsSample.Shared/Options/MyTestOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SnsSample.Shared.Options
+{
+	public class MyTestOptions
+	{
+		public static readonly string SectionName = "MyTest";
+
+		public string? MyTestKey { get; set; }
+	}
+}
---
+++ src/SnsSample.Shared/WebApplicationBuilderExtentions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SnsSample.Shared.Options;
+
+namespace SnsSample.Shared;
+public static class WebApplicationBuilderExtentions
+{
+    public static void AddSharedSettings(this WebApplicationBuilder builder)
+    {
+        IWebHostEnvironment env = builder.Environment;
+        ConfigurationManager config = builder.Configuration;
+        IServiceCollection services = builder.Services;
+
+        var sharedFolder = Path.Combine(env.ContentRootPath, "..", "SnsSample.Shared");
+
+        config.AddJsonFile(Path.Combine(sharedFolder, "sharedsettings.json"), optional: true);
+        config.AddJsonFile(Path.Combine(sharedFolder, $"sharedsettings.{env.EnvironmentName}.json"), optional: true);
+        config.AddJsonFile("sharedsettings.json", optional: true); // publishされた際に参照
+        config.AddJsonFile($"sharedsettings.{env.EnvironmentName}.json", optional: true);
+        config.AddJsonFile("appsettings.json", optional: true); // publishされた際に参照
+        config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+        //usage: Referencing IOptions<MyTestOptions> with dependency injection
+        services.AddOptions<MyTestOptions>().Bind(config.GetSection(MyTestOptions.SectionName));
+    }
+}
