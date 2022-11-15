b4e0425f485e9b8f9788c6e9b67159ef12c58e67
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2022-11-16 03:41:20 +0900

    共通の設定ファイル参照できるようにする #9 (#10)
    * 共通の設定ファイル参照できるようにする #9
    
    * 共通の設定ファイル参照できるようにする #9

--- src/SnsSample.Shared/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace SnsSample.Shared;
-public class Class1
-{
-
-}
---
+++ src/SnsSample.Shared/Options/MyTestOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace SnsSample.Shared.Options
+{
+    public class MyTestOptions
+    {
+        public static readonly string SectionName = "MyTest";
+
+        public string? MyTestKey { get; set; }
+    }
+}
