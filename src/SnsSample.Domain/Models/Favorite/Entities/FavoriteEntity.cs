e422113427f8e8365e876b7878bbbcbb54466b0d
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2022-11-21 23:57:36 +0900

    ドメインモデルを作成する #13

--- src/SnsSample.Domain/Models/Favorite/Entities/FavoriteEntity.cs
+++ src/SnsSample.Domain/Models/Favorite/Entities/FavoriteEntity.cs
@@ -2,20 +2,18 @@
 using SnsSample.Domain.Models.Article.ValueObjects;
 using SnsSample.Domain.Models.Favorite.ValueObjects;
 
-namespace SnsSample.Domain.Models.Favorite.Entities
-{
-    public class FavoriteEntity
-	{
-        public FavoriteId FavoriteId { get; init; }
-        public AccountId AccountId { get; init; }
-        public ArticleId ArticleId { get; init; }
+namespace SnsSample.Domain.Models.Favorite.Entities;
 
-        public FavoriteEntity(FavoriteId favoriteId, AccountId accountId, ArticleId articleId)
-		{
-            FavoriteId = favoriteId;
-            AccountId = accountId;
-            ArticleId = articleId;
-        }
+public class FavoriteEntity
+{
+    public FavoriteId FavoriteId { get; init; }
+    public AccountId AccountId { get; init; }
+    public ArticleId ArticleId { get; init; }
+
+    public FavoriteEntity(FavoriteId favoriteId, AccountId accountId, ArticleId articleId)
+    {
+        FavoriteId = favoriteId;
+        AccountId = accountId;
+        ArticleId = articleId;
     }
 }
-
