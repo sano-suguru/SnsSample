3a2bc4be8ec05babbd85d7702f10b1c3bd3bff13
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2022-11-23 03:53:21 +0900

    ハッシュ化パスワードで英数字のみ受け付けるようにする検証する #15

--- src/SnsSample.Domain/Models/Account/ValueObjects/Hashed.cs
+++ src/SnsSample.Domain/Models/Account/ValueObjects/Hashed.cs
@@ -1,4 +1,6 @@
-﻿namespace SnsSample.Domain.Models.Account.ValueObjects;
+﻿using System.Text.RegularExpressions;
+
+namespace SnsSample.Domain.Models.Account.ValueObjects;
 
 public readonly record struct Hashed
 {
@@ -15,6 +17,11 @@ public readonly record struct Hashed
         if (value.Length > maxLength)
         {
             throw new ArgumentOutOfRangeException(nameof(value), $"'{nameof(value)}' を {maxLength} を超える値にすることはできません。");
+        }
+
+        if (!Regex.IsMatch(value, @"^[0-9a-zA-Z]+$"))
+        {
+            throw new ArgumentException($"'{nameof(value)}' を 半角英数字以外の値にすることはできません。", nameof(value));
         }
 
         Value = value;
