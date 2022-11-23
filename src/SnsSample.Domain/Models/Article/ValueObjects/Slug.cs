ff3543b97251b0e5eab379f6046f6316adcd57fc
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2022-11-23 22:26:41 +0900

    記事のスラッグが半角英数字のみ受け付けるようにする #17

--- src/SnsSample.Domain/Models/Article/ValueObjects/Slug.cs
+++ src/SnsSample.Domain/Models/Article/ValueObjects/Slug.cs
@@ -1,22 +1,29 @@
-﻿namespace SnsSample.Domain.Models.Article.ValueObjects;
+﻿using System.Text.RegularExpressions;
+
+namespace SnsSample.Domain.Models.Article.ValueObjects;
 
 public readonly record struct Slug
 {
     public string Value { get; }
     private static readonly int MaxLength = 26;
 
-    public Slug(string Value)
+    public Slug(string value)
     {
-        if (string.IsNullOrWhiteSpace(Value))
+        if (string.IsNullOrWhiteSpace(value))
         {
-            throw new ArgumentNullException(nameof(Value), $"'{nameof(Value)}' を null または空白にすることはできません。");
+            throw new ArgumentNullException(nameof(value), $"'{nameof(value)}' を null または空白にすることはできません。");
         }
 
-        if (Value.Length > MaxLength)
+        if (value.Length > MaxLength)
         {
-            throw new ArgumentOutOfRangeException(nameof(Value), $"'{nameof(Value)}' を {MaxLength}を超える値にすることはできません。");
+            throw new ArgumentOutOfRangeException(nameof(value), $"'{nameof(value)}' を {MaxLength}を超える値にすることはできません。");
         }
 
-        this.Value = Value;
+        if (!Regex.IsMatch(value, @"^[0-9a-zA-Z]+$"))
+        {
+            throw new ArgumentException($"'{nameof(value)}' を 半角英数字以外の値にすることはできません。", nameof(value));
+        }
+
+        this.Value = value;
     }
 }
