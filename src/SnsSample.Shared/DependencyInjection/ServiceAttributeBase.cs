d23387873a69fe4a6071cbe3a88b7598272bf295
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2022-12-01 01:56:06 +0900

    属性ベースの依存性注入を可能にする #27

---
+++ src/SnsSample.Shared/DependencyInjection/ServiceAttributeBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SnsSample.Shared.DependencyInjection;
+
+public abstract class ServiceAttributeBase
+{
+    public Type? ImplType { get; }
+    public Type? ServiceType { get; }
+    public ServiceLifetime LifeTime { get; }
+    public bool MultipleImplementation { get; }
+
+    protected ServiceAttributeBase(
+        Type? implType
+        , Type? serviceType
+        , ServiceLifetime lifeTime
+        , bool multipleImplementation)
+    {
+        ImplType = implType;
+        ServiceType = serviceType;
+        LifeTime = lifeTime;
+        MultipleImplementation = multipleImplementation;
+    }
+}
---
+++ src/SnsSample.Shared/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace SnsSample.Shared.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddServicesInAssembly(this IServiceCollection services, Assembly targetAssembly)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(targetAssembly);
+
+        var targets = targetAssembly.GetTypes()
+            .Select(t => (type: t, attr: t.GetCustomAttributes().OfType<ServiceAttributeBase>().FirstOrDefault()))
+            .Where(p => p.attr != null);
+
+        foreach ((Type type, ServiceAttributeBase? attr) in targets)
+        {
+            var descriptor = ServiceDescriptor.Describe(attr!.ServiceType ?? type, attr.ImplType ?? type, attr.LifeTime);
+
+            if (!attr.MultipleImplementation)
+            {
+                services.TryAdd(descriptor);
+            }
+            else
+            {
+                services.Add(descriptor);
+            }
+        }
+
+        return services;
+    }
+}
