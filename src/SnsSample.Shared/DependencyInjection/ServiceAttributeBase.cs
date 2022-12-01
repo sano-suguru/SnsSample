d179b2dd6a1b89f9c01b29a6d2dc2fd380e44eed
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2022-12-02 00:27:29 +0900

    属性ベースの依存性注入を可能にする #27 (#30)
    * 属性ベースの依存性注入を可能にする #27
    
    * 属性ベースの依存性注入を可能にする #27
    
    * 属性ベースの依存性注入を可能にする #27

---
+++ src/SnsSample.Shared/DependencyInjection/ServiceAttributeBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SnsSample.Shared.DependencyInjection;
+
+public abstract class ServiceAttributeBase : Attribute
+{
+    public Type? ImplType { get; }
+    public Type? ServiceType { get; }
+    public ServiceLifetime LifeTime { get; }
+    public bool MultipleImplementation { get; }
+
+    protected ServiceAttributeBase(
+        Type? implType
+        , Type? serviceType
+        , ServiceLifetime lifeTime
+        , bool multipleImplementation)
+    {
+        ImplType = implType;
+        ServiceType = serviceType;
+        LifeTime = lifeTime;
+        MultipleImplementation = multipleImplementation;
+    }
+}
