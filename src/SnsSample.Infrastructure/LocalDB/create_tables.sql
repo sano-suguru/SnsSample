250edf0cea8e3a4cbdac0e57058fdee022e0ba21
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2022-12-03 00:49:19 +0900

    SQLiteのDBをとテーブルを作成する #29

--- src/SnsSample.Infrastructure/LocalDB/create_tables.sql
+++ src/SnsSample.Infrastructure/LocalDB/create_tables.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "Account" (
 	"AccountId"	INTEGER NOT NULL UNIQUE,
+	"Code"	TEXT NOT NULL UNIQUE,
 	"NIckname"	TEXT NOT NULL,
 	"Introduction"	TEXT,
 	"Location"	TEXT,
@@ -10,4 +11,29 @@ CREATE TABLE "Account" (
 	"CreatedAt"	TEXT NOT NULL,
 	"UpdatedAt"	INTEGER,
 	PRIMARY KEY("AccountId" AUTOINCREMENT)
+)
+
+CREATE INDEX "IX_Account_Code" ON "Account" (
+	"Code"
+)
+
+CREATE INDEX "IX_Account_CreatedAt" ON "Account" (
+	"CreatedAt"
+)
+
+CREATE INDEX "IX_Account_Nickname" ON "Account" (
+	"NIckname"
+)
+CREATE TABLE "Friendship" (
+	"FriendshipId"	INTEGER NOT NULL UNIQUE,
+	"Following"	INTEGER NOT NULL,
+	"Followed"	INTEGER NOT NULL,
+	FOREIGN KEY("Followed") REFERENCES "Account"("AccountId"),
+	FOREIGN KEY("Following") REFERENCES "Account"("AccountId"),
+	PRIMARY KEY("FriendshipId" AUTOINCREMENT)
+)
+
+CREATE INDEX "IX_Friendship_Following_Followed" ON "Friendship" (
+	"Following",
+	"Followed"
 )
