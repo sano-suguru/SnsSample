9bca11d42863737e537d1d9e5f4abaac0fdc2735
Sano Suguru <43309177+sano-suguru@users.noreply.github.com>
2022-12-06 00:44:03 +0900

    アカウントプロファイルのドメインモデルを作成する #41

--- src/SnsSample.Domain/Models/Account/Entities/AccountEntity.cs
+++ src/SnsSample.Domain/Models/Account/Entities/AccountEntity.cs
@@ -6,32 +6,17 @@ public class AccountEntity
 {
     public AccountId AccountId { get; init; }
     public Code Code { get; init; }
-    public Nickname Nickname { get; init; }
-    public Introduction Introduction { get; init; }
-    public Location Location { get; init; }
-    public WebSite WebSite { get; init; }
-    public Birthday Birthday { get; init; }
     public Salt Salt { get; init; }
     public Hashed Hashed { get; init; }
 
     public AccountEntity(
         AccountId accountId,
         Code code,
-        Nickname nickname,
-        Introduction introduction,
-        Location location,
-        WebSite webSite,
-        Birthday birthday,
         Salt salt,
         Hashed hashed)   
     {
         AccountId = accountId;
         Code = code;
-        Nickname = nickname;
-        Introduction = introduction;
-        Location = location;
-        WebSite = webSite;
-        Birthday = birthday;
         Salt = salt;
         Hashed = hashed;
     }
